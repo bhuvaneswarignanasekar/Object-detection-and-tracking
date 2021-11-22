6e6fda8abe3cc35b1e9d25447b383a881b7fb64a
bhuvaneswarignanasekar <59138503+bhuvaneswarignanasekar@users.noreply.github.com>
2021-11-22 08:51:23 -0600

    Update bouncing_ball_opencv.py

--- bouncing_ball_opencv.py
+++ bouncing_ball_opencv.py
@@ -5,21 +5,27 @@ img = np.zeros((480,640,3),dtype='uint8')
 dx,dy =1,1
 x,y = 100,100
 while True:
-    # Display the image
-    cv2.imshow('a',img)
+    # display frame
+    cv2.imshow('bouncing_ball',img)
     k = cv2.waitKey(10)
     img = np.zeros((480,640,3),dtype='uint8') 
+
     # Increment the position
     x = x+dx
     y = y+dy
+
+    #create a ball
     cv2.circle(img,(x,y),20,(255,0,0),-1)
     if k != -1:
         break
-    # Change the sign of increment on collision with the boundary
+
+    # change y axis
     if y >=480:
         dy *= -1
     elif y<=0:
         dy *= -1
+
+    #change x asix
     if x >=640:
         dx *= -1
     elif x<=0:
